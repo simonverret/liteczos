fa653e889d08a011ceca51a0debdde1d0458ea5d
Simon Verret <verret.simon@gmail.com>
2025-02-06 20:21:33 -0500

    Removed second_quantized.py

--- tests/test_second_quantized.py
+++
@@ -1,6 +0,0 @@
-import pytest
-import numpy as np
-from liteczos import twosites, second_quantized
-
-def test_number_operator():
-    assert np.allclose(twosites.number(), second_quantized.number(num_qbits=4))
